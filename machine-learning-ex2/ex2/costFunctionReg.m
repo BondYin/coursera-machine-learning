b33f0af3a4a6efa2ba6524e1a76b1d605af1c0ad
Max Poon <maxpanziyuan@gmail.com>
2016-12-28 14:58:23 +0800

    finish ex2

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,8 +18,10 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
-
-
+h = sigmoid(X*theta);
+J = (-1/m)*(y'*log(h) + (1-y)'*log(1-h)) + lambda/(2*m) * ((theta(2:end)'*theta(2:end)) );
+grad = (1/m)*X'*(h-y) + lambda/m * theta;
+grad(1) = grad(1) - lambda/m * theta(1);
 
 
 % =============================================================
